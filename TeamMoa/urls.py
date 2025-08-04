7c1a2695bbc8cef80ab2e7cf01d1069cd7a5cf95
TlesMes <56552726+TlesMes@users.noreply.github.com>
2025-08-05 04:39:11 +0900

    메인 페이지 경로를 'accounts/'로 변경 (임시 설정)

--- TeamMoa/urls.py
+++ TeamMoa/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/accounts/', permanent=False)),
     path('accounts/', include('accounts.urls')),
     path('teams/', include('teams.urls')),
     path('shares/',include('shares.urls')),
