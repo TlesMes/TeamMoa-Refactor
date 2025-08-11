839f64803697abc8c799eb73cc956cd9b4548921
TlesMes <56552726+TlesMes@users.noreply.github.com>
2025-08-12 00:49:46 +0900

    view, services, form 책임 분리
    메일 전송 인코딩 수정중

--- accounts/services.py
+++ accounts/services.py
@@ -3,32 +3,17 @@ from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
 from django.core.mail import EmailMessage
-from django.db import IntegrityError
-import re
-
-from .models import User
+from accounts.forms import SignupForm
 from .tokens import account_activation_token
 
 
-def register_user(username, email, password, nickname, profile, current_site):
+def register_user(form:SignupForm, current_site):
     """
     사용자를 등록하고 활성화 이메일을 발송합니다.
     """
-    # 이메일 형식 검사
-    REGEX_EMAIL = r'^[\w\.-]+@[\w\.-]+\.\w+$'
-
-    if not re.fullmatch(REGEX_EMAIL, email):
-        raise ValueError("이메일 형식이 맞지 않습니다.")
-
-    try:
-        # 유저 생성 및 필드 설정
-        user = User.objects.create_user(username=username, email=email, password=password)
-        user.is_active = False
-        user.nickname = nickname
-        user.profile = profile
-        user.save()
-    except IntegrityError:
-        raise IntegrityError("존재하는 계정입니다.")
+    # 유저 생성 및 필드 설정    
+    user = form.save(commit=False)
+    user.is_active = False
 
     # 인증 메일 전송
     message = render_to_string('accounts/user_activate_email.html', {
@@ -38,7 +23,8 @@ def register_user(username, email, password, nickname, profile, current_site):
         'token': account_activation_token.make_token(user),
     })
     mail_subject = "[TeamMoa] 회원가입 인증 메일입니다."
-    email_message = EmailMessage(mail_subject, message, to=[email])
+    email_message = EmailMessage(mail_subject, message, to=[user.email])
     email_message.send()
 
+    user.save()
     return user
