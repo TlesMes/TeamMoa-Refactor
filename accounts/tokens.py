da2b2ce5de8484888f3a6cec552d4bdde003bb62
TlesMes <56552726+TlesMes@users.noreply.github.com>
2025-08-21 03:39:45 +0900

    six 라이브러리 제거

--- accounts/tokens.py
+++ accounts/tokens.py
@@ -1,10 +1,9 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
-from six import text_type
 
 
 class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, profile, timestamp):
-        return (text_type(profile.id)) + text_type(timestamp)
+        return str(profile.id) + str(timestamp)
 
 
 account_activation_token = AccountActivationTokenGenerator()
