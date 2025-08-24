7f0ce890dfbcfdd38e80b672c2c53492694fc7cd
TlesMes <56552726+TlesMes@users.noreply.github.com>
2025-08-24 21:50:09 +0900

    mindmaps/views.py CBV 리팩토링

--- mindmaps/views.py
+++ mindmaps/views.py
@@ -1,146 +1,235 @@
-from django.http import HttpResponseRedirect,HttpResponse
-from django.shortcuts import render,redirect,get_object_or_404
+from django.http import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
+from django.views.generic import TemplateView, ListView, DetailView, DeleteView, FormView
+from django.views import View
+from django.contrib import messages
+from django.urls import reverse
 
-from teams.models import TeamUser
+from teams.models import TeamUser, Team
 from .forms import CreateMindmapForm
-from teams.models import Team
 from .models import Comment, Mindmap, Node, NodeConnection, NodeUser
 from accounts.models import User
+from common.mixins import TeamMemberRequiredMixin, TeamHostRequiredMixin
 import logging
-def is_member(request, pk) -> bool:
-    user = request.user
-    if not user.is_authenticated:
-        return redirect('/accounts/login')
 
-    if user.is_authenticated:
-        team = get_object_or_404(Team, pk=pk)
-        if user in team.members.all():
-            return True
-        else:
-            return False
 
 # Create your views here.
-def mindmap_list_page(request, pk):
-    if not is_member(request, pk):
-        return HttpResponse('<script>alert("팀원이 아닙니다.")</script>''<script>location.href="/teams/"</script>')
-
-    user = request.user
-    team = get_object_or_404(Team, pk=pk)
-    mindmaps = Mindmap.objects.filter(team=team)
-  
-    return render(request, 'mindmaps/mindmap_list_page.html', {'mindmaps':mindmaps, 'team':team})
-
-def mindmap_detail_page(request, pk, mindmap_id):
-    if not is_member(request, pk):
-        return HttpResponse('<script>alert("팀원이 아닙니다.")</script>''<script>location.href="/teams/"</script>')
+class MindmapListPageView(TeamMemberRequiredMixin, ListView):
+    model = Mindmap
+    template_name = 'mindmaps/mindmap_list_page.html'
+    context_object_name = 'mindmaps'
     
-    user = request.user
-    team = get_object_or_404(Team, pk=pk)
-    mindmap = Mindmap.objects.get(pk=mindmap_id)
-    nodes = Node.objects.filter(mindmap=mindmap)
-    lines = NodeConnection.objects.filter(mindmap=mindmap)
-    return render(request, 'mindmaps/mindmap_detail_page.html',{'nodes':nodes,'team':team,'mindmap':mindmap, 'lines':lines})
+    def get_queryset(self):
+        team = get_object_or_404(Team, pk=self.kwargs['pk'])
+        return Mindmap.objects.filter(team=team).order_by('-id')
     
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        team = get_object_or_404(Team, pk=self.kwargs['pk'])
+        context['team'] = team
+        return context
 
-def mindmap_create(request, pk):
-    user = request.user
-    team = get_object_or_404(Team, pk=pk)
-    if request.method =='POST':
-        form = CreateMindmapForm(request.POST)
-        if form.is_valid():
-            mindmap = Mindmap()
-            mindmap.title = form.cleaned_data['title']
-            mindmap.team = team
-            mindmap.save()
-            return redirect(f'/mindmaps/mindmap_list_page/{pk}')
-    else:
-        form = CreateMindmapForm()
-    return render(request, 'mindmaps/mindmap_create.html',{'form':form, 'team':team})
 
-def mindmap_delete(request, pk, mindmap_id):
-    team = get_object_or_404(Team, pk=pk)
-    mindmap = get_object_or_404(Mindmap, pk=mindmap_id)
+class MindmapDetailPageView(TeamMemberRequiredMixin, DetailView):
+    model = Mindmap
+    template_name = 'mindmaps/mindmap_detail_page.html'
+    context_object_name = 'mindmap'
+    pk_url_kwarg = 'mindmap_id'
     
-    # 팀장만 마인드맵을 삭제할 수 있음
-    if request.user == team.host:
-        mindmap.delete()
-        return redirect('mindmaps:mindmap_list_page', pk=pk)
-    else:
-        return HttpResponse('<script>alert("팀장만 마인드맵을 삭제할 수 있습니다.")</script><script>history.back()</script>')
-
-def mindmap_create_node(request, pk, mindmap_id):
-    team = get_object_or_404(Team, pk=pk)
-    mindmap=Mindmap.objects.get(pk=mindmap_id)
-    if request.method =='POST':
-        node = Node()
-        node.posX = request.POST["posX"]
-        node.posY = request.POST["posY"]
-        node.title = request.POST["title"]
-        node.content = request.POST["content"]
-        node.mindmap = mindmap
-        node.save()
-        members=TeamUser.objects.filter(team=team)
-        for member in members:
-            node.user.add(member.user)
-        parent=request.POST["parent"]
-        if parent != "":
-            parentnode = Node.objects.get(title=parent,mindmap=mindmap)
-            NodeConnection.objects.create(from_node=node,to_node=parentnode,mindmap=mindmap)
-
-    return redirect(f'/mindmaps/mindmap_detail_page/{pk}/{mindmap_id}')
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        team = get_object_or_404(Team, pk=self.kwargs['pk'])
+        mindmap = self.get_object()
+        nodes = Node.objects.filter(mindmap=mindmap).order_by('id')
+        lines = NodeConnection.objects.filter(mindmap=mindmap).order_by('id')
+        
+        context.update({
+            'team': team,
+            'nodes': nodes,
+            'lines': lines
+        })
+        return context
 
 
-def mindmap_delete_node(request, pk, node_id):
-    team = get_object_or_404(Team, pk=pk)
-    node = Node.objects.get(pk=node_id)
-    node.delete()
-    return redirect(f'/mindmaps/mindmap_detail_page/{pk}/{node.mindmap.id}')
-
-def mindmap_empower(request, pk, mindmap_id, user_id):
-    pass
-
-def node_detail_page(request, pk, node_id):
-    team = get_object_or_404(Team, pk=pk)
-    node = Node.objects.get(pk=node_id)
-    if request.method =='POST':
-        user = request.user
-        comment = Comment()
-        comment.comment = request.POST["comment"]
-        comment.node = node
-        comment.user = user
-        comment.save()
-    comments = Comment.objects.filter(node=node)
-    return render(request, 'mindmaps/node_detail_page.html',{'node':node, 'team':team,'comments':comments})
+class MindmapCreateView(TeamMemberRequiredMixin, FormView):
+    form_class = CreateMindmapForm
+    template_name = 'mindmaps/mindmap_create.html'
     
-def node_vote(request, pk, node_id):
-    user = request.user
-    team = get_object_or_404(Team, pk=pk)
-    node = Node.objects.get(pk=node_id)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        team = get_object_or_404(Team, pk=self.kwargs['pk'])
+        context['team'] = team
+        return context
     
-    # 중복 NodeUser 처리를 위한 안전한 조회
-    try:
-        nodeuser = NodeUser.objects.get(node=node, user=user)
-    except NodeUser.MultipleObjectsReturned:
-        # 중복이 있으면 첫 번째 것을 사용하고 나머지는 삭제
-        nodeuser = NodeUser.objects.filter(node=node, user=user).first()
-        NodeUser.objects.filter(node=node, user=user).exclude(pk=nodeuser.pk).delete()
-    except NodeUser.DoesNotExist:
-        # NodeUser가 없으면 생성
-        nodeuser = NodeUser.objects.create(node=node, user=user, voted=False)
+    def form_valid(self, form):
+        team = get_object_or_404(Team, pk=self.kwargs['pk'])
+        mindmap = Mindmap.objects.create(
+            title=form.cleaned_data['title'],
+            team=team
+        )
+        messages.success(self.request, f'마인드맵 "{mindmap.title}"가 성공적으로 생성되었습니다.')
+        return redirect('mindmaps:mindmap_list_page', pk=self.kwargs['pk'])
+
+
+class MindmapDeleteView(TeamHostRequiredMixin, DeleteView):
+    model = Mindmap
+    pk_url_kwarg = 'mindmap_id'
     
-    comments = Comment.objects.filter(node=node)
-    if nodeuser.voted:
-        node.vote = node.vote - 1
-    else:
-        node.vote = node.vote + 1
-    node.save()
-    nodeuser.voted = not nodeuser.voted
-    nodeuser.save()
-
-    return render(request, 'mindmaps/node_detail_page.html',{'node':node, 'team':team,'comments':comments})
+    def get_success_url(self):
+        messages.success(self.request, '마인드맵이 성공적으로 삭제되었습니다.')
+        return reverse('mindmaps:mindmap_list_page', kwargs={'pk': self.kwargs['pk']})
 
 
-def node_add_comment(request):
-    pass
+class MindmapCreateNodeView(TeamMemberRequiredMixin, TemplateView):
+    def post(self, request, pk, mindmap_id, *args, **kwargs):
+        team = get_object_or_404(Team, pk=pk)
+        mindmap = get_object_or_404(Mindmap, pk=mindmap_id)
+        
+        # 필수 필드 검증
+        required_fields = ['posX', 'posY', 'title', 'content']
+        for field in required_fields:
+            if not request.POST.get(field):
+                messages.error(request, f'{field} 필드는 필수입니다.')
+                return redirect('mindmaps:mindmap_detail_page', pk=pk, mindmap_id=mindmap_id)
+        
+        try:
+            # 노드 생성
+            node = Node.objects.create(
+                posX=int(request.POST["posX"]),
+                posY=int(request.POST["posY"]),
+                title=request.POST["title"],
+                content=request.POST["content"],
+                mindmap=mindmap
+            )
+            
+            # 팀 멤버를 노드에 자동 추가
+            members = TeamUser.objects.filter(team=team)
+            for member in members:
+                node.user.add(member.user)
+            
+            # 부모 노드 연결 (선택사항)
+            parent_title = request.POST.get("parent")
+            if parent_title:
+                try:
+                    parent_node = Node.objects.get(title=parent_title, mindmap=mindmap)
+                    NodeConnection.objects.create(from_node=node, to_node=parent_node, mindmap=mindmap)
+                except Node.DoesNotExist:
+                    messages.warning(request, '부모 노드를 찾을 수 없어 연결이 생성되지 않았습니다.')
+            
+            messages.success(request, f'노드 "{node.title}"이 성공적으로 생성되었습니다.')
+            
+        except (ValueError, TypeError) as e:
+            messages.error(request, '잘못된 입력값입니다. 위치 정보는 숫자여야 합니다.')
+        except Exception as e:
+            logging.error(f'노드 생성 오류: {e}')
+            messages.error(request, '노드 생성 중 오류가 발생했습니다.')
+        
+        return redirect('mindmaps:mindmap_detail_page', pk=pk, mindmap_id=mindmap_id)
 
+
+class MindmapDeleteNodeView(TeamMemberRequiredMixin, DeleteView):
+    model = Node
+    pk_url_kwarg = 'node_id'
+    
+    def get_success_url(self):
+        node = self.get_object()
+        messages.success(self.request, f'노드 "{node.title}"이 성공적으로 삭제되었습니다.')
+        return reverse('mindmaps:mindmap_detail_page', kwargs={
+            'pk': self.kwargs['pk'], 
+            'mindmap_id': node.mindmap.id
+        })
+
+
+class NodeDetailPageView(TeamMemberRequiredMixin, DetailView):
+    model = Node
+    template_name = 'mindmaps/node_detail_page.html'
+    context_object_name = 'node'
+    pk_url_kwarg = 'node_id'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        team = get_object_or_404(Team, pk=self.kwargs['pk'])
+        node = self.get_object()
+        comments = Comment.objects.filter(node=node).order_by('-id')
+        
+        context.update({
+            'team': team,
+            'comments': comments
+        })
+        return context
+    
+    def post(self, request, *args, **kwargs):
+        node = self.get_object()
+        comment_text = request.POST.get("comment")
+        
+        if not comment_text or not comment_text.strip():
+            messages.error(request, '댓글 내용을 입력해주세요.')
+        else:
+            Comment.objects.create(
+                comment=comment_text.strip(),
+                node=node,
+                user=request.user
+            )
+            messages.success(request, '댓글이 성공적으로 등록되었습니다.')
+        
+        return redirect('mindmaps:node_detail_page', pk=self.kwargs['pk'], node_id=node.id)
+
+
+class NodeVoteView(TeamMemberRequiredMixin, View):
+    def post(self, request, pk, node_id, *args, **kwargs):
+        node = get_object_or_404(Node, pk=node_id)
+        
+        try:
+            # 기존 투표 확인
+            nodeuser = NodeUser.objects.get(node=node, user=request.user)
+            
+            # 투표 상태 토글
+            if nodeuser.vote:
+                nodeuser.vote = False
+                vote_action = "취소"
+            else:
+                nodeuser.vote = True  
+                vote_action = "추가"
+            nodeuser.save()
+            
+        except NodeUser.DoesNotExist:
+            # 투표 기록이 없으면 새로 생성 (투표)
+            NodeUser.objects.create(node=node, user=request.user, vote=True)
+            vote_action = "추가"
+            
+        except NodeUser.MultipleObjectsReturned:
+            # 중복 데이터 처리
+            nodeuser = NodeUser.objects.filter(node=node, user=request.user).first()
+            NodeUser.objects.filter(node=node, user=request.user).exclude(pk=nodeuser.pk).delete()
+            logging.warning(f'NodeUser 중복 제거: node_id={node_id}, user_id={request.user.id}')
+            
+            # 투표 상태 토글
+            nodeuser.vote = not nodeuser.vote
+            nodeuser.save()
+            vote_action = "추가" if nodeuser.vote else "취소"
+        
+        # 현재 득표수 계산
+        current_votes = NodeUser.objects.filter(node=node, vote=True).count()
+        
+        messages.success(request, f'투표가 {vote_action}되었습니다. (현재: {current_votes}표)')
+        return redirect('mindmaps:node_detail_page', pk=pk, node_id=node_id)
+
+
+class MindmapEmpowerView(TeamMemberRequiredMixin, View):
+    def post(self, request, pk, mindmap_id, user_id, *args, **kwargs):
+        # 구현되지 않은 기능으로 보임 - 향후 구현
+        messages.info(request, '이 기능은 아직 구현되지 않았습니다.')
+        return redirect('mindmaps:mindmap_detail_page', pk=pk, mindmap_id=mindmap_id)
+
+
+# 하위 호환성을 위한 함수형 뷰 래퍼
+mindmap_list_page = MindmapListPageView.as_view()
+mindmap_detail_page = MindmapDetailPageView.as_view()
+mindmap_create = MindmapCreateView.as_view()
+mindmap_delete = MindmapDeleteView.as_view()
+mindmap_create_node = MindmapCreateNodeView.as_view()
+mindmap_delete_node = MindmapDeleteNodeView.as_view()
+mindmap_empower = MindmapEmpowerView.as_view()
+node_detail_page = NodeDetailPageView.as_view()
+node_vote = NodeVoteView.as_view()
