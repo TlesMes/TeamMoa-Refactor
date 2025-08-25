c280cfe367ff0fc704b5aad5a08b0e77a35f91e9
TlesMes <56552726+TlesMes@users.noreply.github.com>
2025-08-25 19:11:38 +0900

    members/view.py CBV 리팩토링

--- members/views.py
+++ members/views.py
@@ -1,57 +1,62 @@
-from django.shortcuts import render,redirect,get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import TemplateView
+from django.views import View
 from members.forms import CreateTodoForm
 from members.models import Todo
 from teams.models import Team, TeamUser
-from django.http import HttpResponseRedirect,HttpResponse
-# Create your views here.
+from django.http import HttpResponseRedirect, HttpResponse
+from common.mixins import TeamMemberRequiredMixin
 
-def is_member(request, pk) -> bool:
-    user = request.user
-    if not user.is_authenticated:
-        return redirect('/accounts/login')
-    team = get_object_or_404(Team, pk=pk)
-    if user in team.members.all():
-        return True
-    else:
-        return False
-
-
-def team_members_page(request, pk):
-    if not is_member(request, pk):
-        return HttpResponse('<script>alert("팀원이 아닙니다.")</script>''<script>location.href="/teams/"</script>')
-    else:
-        team = get_object_or_404(Team, pk=pk)
+class TeamMembersPageView(TeamMemberRequiredMixin, TemplateView):
+    template_name = 'members/team_members_page.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        team = get_object_or_404(Team, pk=kwargs['pk'])
         members = TeamUser.objects.filter(team=team)
         todos = Todo.objects.filter(owner__team=team)
-        if request.method =='POST':
-            form = CreateTodoForm(request.POST)
-            if form.is_valid():
-                member_add_Todo(request, pk ,form.cleaned_data['content'])
-            return redirect(f'/members/team_members_page/{pk}')
-        
         form = CreateTodoForm()
-        return render(request, 'members/team_members_page.html', {'members':members, 'todos':todos, 'team':team, 'form':form})
-
-def member_add_Todo(request, pk, content):
-    user = request.user
-    team = get_object_or_404(Team, pk=pk)
-    teamuser = TeamUser.objects.get(team=team, user=user)
-    todo = Todo()
-    todo.content = content
-    todo.owner = teamuser
-    todo.save()
+        
+        context.update({
+            'team': team,
+            'members': members,
+            'todos': todos,
+            'form': form
+        })
+        return context
     
-def member_complete_Todo(request, pk, todo_id):
-    user = request.user
-    team = get_object_or_404(Team, pk=pk)
-    todo = Todo.objects.get(pk=todo_id)
-    todo.is_completed = not todo.is_completed
-    todo.save()
-    return redirect(f'/members/team_members_page/{pk}')
+    def post(self, request, pk):
+        form = CreateTodoForm(request.POST)
+        if form.is_valid():
+            self.member_add_todo(request, pk, form.cleaned_data['content'])
+        return redirect('members:team_members_page', pk=pk)
+    
+    def member_add_todo(self, request, pk, content):
+        user = request.user
+        team = get_object_or_404(Team, pk=pk)
+        teamuser = TeamUser.objects.get(team=team, user=user)
+        todo = Todo()
+        todo.content = content
+        todo.owner = teamuser
+        todo.save()
 
-def member_delete_Todo(request, pk, todo_id):
-    user = request.user
-    team = get_object_or_404(Team, pk=pk)
-    todo = Todo.objects.get(pk=todo_id)
-    todo.delete()
-    return redirect(f'/members/team_members_page/{pk}')
+
+team_members_page = TeamMembersPageView.as_view()
+
+class MemberCompleteTodoView(TeamMemberRequiredMixin, View):
+    def get(self, request, pk, todo_id):
+        todo = get_object_or_404(Todo, pk=todo_id)
+        todo.is_completed = not todo.is_completed
+        todo.save()
+        return redirect('members:team_members_page', pk=pk)
+
+
+class MemberDeleteTodoView(TeamMemberRequiredMixin, View):
+    def get(self, request, pk, todo_id):
+        todo = get_object_or_404(Todo, pk=todo_id)
+        todo.delete()
+        return redirect('members:team_members_page', pk=pk)
+
+
+member_complete_Todo = MemberCompleteTodoView.as_view()
+member_delete_Todo = MemberDeleteTodoView.as_view()
