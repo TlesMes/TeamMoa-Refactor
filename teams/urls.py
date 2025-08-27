a27d913a35c81ef4868f1255410d65e0e537a714
TlesMes <56552726+TlesMes@users.noreply.github.com>
2025-08-28 02:29:56 +0900

    milestone 페이지 타임라인 기본 기능 구현

--- teams/urls.py
+++ teams/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
     path('team_info_change/<int:pk>/', views.team_info_change, name='team_info_change'),
     path('team_main_page/<int:pk>/', views.team_main_page, name='team_main_page'),
     path('team_add_milestone/<int:pk>/', views.team_add_milestone, name='team_add_milestone'),
+    path('team_milestone_timeline/<int:pk>/', views.team_milestone_timeline, name='team_milestone_timeline'),
+    path('team/<int:pk>/milestone/<int:milestone_id>/update/', views.milestone_update_ajax, name='milestone_update_ajax'),
     path('team_delete_milestone/<int:pk>/<int:milestone_id>/', views.team_delete_milestone, name='team_delete_milestone'),
     path('team_disband/<int:pk>/', views.team_disband, name='team_disband')
 ]
